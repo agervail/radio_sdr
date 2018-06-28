31fb8daf18364b88715795f34c000ae3393c78dc
Antoine <antoine@sogilis.com>
2018-06-28 14:04:07 +0200

    replace slow by pandas

--- pyqtgraph/main.py
+++ pyqtgraph/main.py
@@ -5,8 +5,6 @@ import pyqtgraph as pg
 import sys
 from signal_sdr import SignalSdr
 
-
-
 class MainWidget(QtGui.QSplitter):
     def __init__(self, data_plot_widget, side_view_widget):
         super().__init__(QtCore.Qt.Horizontal)
@@ -32,7 +30,7 @@ class DataPlotWidget(pg.PlotWidget):
         self.signal_sdr.overlay_changed.connect(self.drawOverlay)
 
     def drawPoints(self):
-        self.plot(self.signal_sdr.points[0], self.signal_sdr.points[1])
+        self.plot(self.signal_sdr.points.index, self.signal_sdr.points['signal'].values)
         self.setLimits(yMin=125, yMax=158, minYRange=33)
 
     def drawOverlay(self):
@@ -44,21 +42,48 @@ class SideViewWidget(QtGui.QWidget):
     def __init__(self, signal_sdr):
         super().__init__()
         self.signal_sdr = signal_sdr
+        self.signal_sdr.freq_changed.connect(self.freq_update)
 
-        self.nb_of_clock = QtGui.QSpinBox()
-        self.nb_of_clock.setRange(1, 1000)
-        self.nb_of_clock.setValue(1)
-        self.nb_of_clock.setPrefix("Number of clock ticks : ")
-        self.nb_of_clock.valueChanged.connect(self.on_nb_of_clock)
+        self.sample_freq = QtGui.QSpinBox()
+        self.sample_freq.setRange(1, 1000000)
+        self.sample_freq.setValue(96000)
+        self.sample_freq.setPrefix("Signal sample freq : ")
+        self.sample_freq.setSuffix("KHz")
+        self.sample_freq.valueChanged.connect(self.on_sample_freq)
 
+        self.nb_of_edges = QtGui.QSpinBox()
+        self.nb_of_edges.setRange(1, 1000)
+        self.nb_of_edges.setValue(1)
+        self.nb_of_edges.setPrefix("Number of edges : ")
+        self.nb_of_edges.valueChanged.connect(self.on_nb_of_edges)
+
+        self.nb_of_ticks_in_selection = QtGui.QSpinBox()
+        self.nb_of_ticks_in_selection.setRange(1, 1000)
+        self.nb_of_ticks_in_selection.setValue(1)
+        self.nb_of_ticks_in_selection.setPrefix("Clock ticks in edges selection : ")
+        self.nb_of_ticks_in_selection.valueChanged.connect(self.on_nb_of_ticks_in_selection)
+
+        self.clock_freq = QtGui.QLabel("clock freq : 0")
 
         layout = QtGui.QGridLayout()
-        layout.addWidget(self.nb_of_clock, 0, 0)
+        layout.addWidget(self.sample_freq, 0, 0)
+        layout.addWidget(self.nb_of_edges, 1, 0)
+        layout.addWidget(self.nb_of_ticks_in_selection, 2, 0)
+        layout.addWidget(self.clock_freq, 3, 0)
 
         self.setLayout(layout)
 
-    def on_nb_of_clock(self):
-        self.signal_sdr.nb_of_clock_change(self.nb_of_clock.value())
+    def freq_update(self):
+        self.clock_freq.setText("clock freq : " + str(self.signal_sdr.clock_freq))
+
+    def on_nb_of_edges(self):
+        self.signal_sdr.nb_of_edges_change(self.nb_of_edges.value())
+
+    def on_nb_of_ticks_in_selection(self):
+        self.signal_sdr.nb_of_ticks_in_selection_change(self.nb_of_ticks_in_selection.value())
+
+    def on_sample_freq(self):
+        self.signal_sdr.sample_freq_change(self.sample_freq.value())
 
 if __name__ == '__main__':
     pg.setConfigOption('background', 'w')
--- pyqtgraph/signal_sdr.py
+++ pyqtgraph/signal_sdr.py
@@ -2,61 +2,69 @@
 # -*- coding: utf-8 -*-
 from PyQt5 import QtCore, QtGui, Qt
 from scipy.io import wavfile
+import pandas as pd
 
 class SignalSdr(QtCore.QObject):
     data_changed = QtCore.pyqtSignal()
     overlay_changed = QtCore.pyqtSignal()
+    freq_changed = QtCore.pyqtSignal()
 
     def __init__(self, filename = None):
         super().__init__()
         self.start = 0
         self.end = 0
-        self.points = [[],[]]
-        self.rising = []
-        self.falling = []
-        self.risfall = []
+        self.points = None
         self.start_tick = 0
         self.end_tick = 0
+        self.clock_freq = 0
+        self.nb_of_ticks_in_selection = 1
+        self.sample_freq = 96000
         self._init_data(filename)
+        self.freq_changed.emit()
 
-    def nb_of_clock_change(self, nb_of_clock):
-        self.end_tick = self.risfall[nb_of_clock]
+    def nb_of_edges_change(self, nb_of_edges):
+        self.end_tick = self.points.index[(self.points['rising'] |
+                                           self.points['falling'])][nb_of_edges]
         self._update_overlay()
-        print(nb_of_clock)
+        self._update_clock_freq()
+        self.freq_changed.emit()
 
+    def nb_of_ticks_in_selection_change(self, nb_of_ticks_in_selection):
+        self.nb_of_ticks_in_selection = nb_of_ticks_in_selection
+        self._update_clock_freq()
+        self.freq_changed.emit()
+
+    def sample_freq_change(self, sample_freq):
+        self.sample_freq = sample_freq
+        self._update_clock_freq()
+        self.freq_changed.emit()
 
     def _update_overlay(self):
         self.overlay_changed.emit()
 
+    def _update_clock_freq(self):
+        one_tick = (self.end_tick - self.start_tick) / self.nb_of_ticks_in_selection
+        self.clock_freq = one_tick * (1 / (self.sample_freq * 1000))
+
     def _init_data(self, filename):
         k = wavfile.read(filename)[1]
+        df = pd.DataFrame(k, columns=['signal'])
+        s_min = df['signal'].min()
+        s_max = df['signal'].max()
+        df = df.clip(s_min + 10, s_max - 10)
+        df = df.loc[(df['signal'].shift() != df['signal'])
+                    | (df['signal'].shift(-1) != df['signal']), ['signal']]
+
+        threshold = s_min + (s_max - s_min) / 2
+
+        df['rising'] = (df['signal'] < threshold) & (df['signal'].shift() >= threshold)
+        df['falling'] = (df['signal'] > threshold) & (df['signal'].shift() <= threshold)
+
         message_max_size = 200000
-        threshold = 150
+        self.start = (min(df['rising'].idxmax(), df['falling'].idxmax()))
+        self.end = (min(df.loc[self.start+message_max_size:self.start:-1]['rising'].idxmax(),
+                        df.loc[self.start+message_max_size:self.start:-1]['falling'].idxmax()))
 
-        self.start = 0
-        for i, val in enumerate(k[1:]):
-            if k[i] < threshold and val >= threshold:
-                if self.start == 0:
-                    self.start = i
-                self.rising.append(i)
-                self.risfall.append(i)
-            if k[i] > threshold and val <= threshold:
-                self.end = i
-                self.falling.append(i)
-                self.risfall.append(i)
-
-        raw = k[self.start - 20:self.start - 20 + message_max_size]
-        rounded = self._round_data(raw)
-
-        for i, val in enumerate(rounded):
-            self.points[0].append(i + self.start - 20)
-            self.points[1].append(val)
-
-        self.start_tick = self.risfall[0]
-        self.end_tick = self.risfall[1]
-
-    def _round_data(self, data):
-        minV = 128
-        maxV = 153
-        middle = minV + (maxV - minV) / 2
-        return [minV if x < middle else maxV for x in data]
+        self.start_tick = df.index[df['rising'] | df['falling']][0]
+        self.end_tick = df.index[df['rising'] | df['falling']][1]
+        self.points = df.loc[self.start:self.end]
